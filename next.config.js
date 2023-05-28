b606b36ae07984515923d2b1853a2331d759d88d
juanedcabrera <juanedcabrera@gmail.com>
2023-05-28 09:13:28 -0600

    Added images to episodes and to thumbnails to the allepisodes grid

--- next.config.js
+++ next.config.js
@@ -1,8 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/vi/**',
+      },
+    ],
+  },
   env: {
     REACT_APP_SERVER_URL: process.env.REACT_APP_SERVER_URL,
   },
 };
 
+
+
 module.exports = nextConfig;
+
+// module.exports = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'i.ytimg.com',
+//         port: '',
+//         pathname: '/vi/**',
+//       },
+//     ],
+//   }, nextConfig
+// };
+
